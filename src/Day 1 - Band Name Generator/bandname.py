c9c83f5409416a1cc3b36f1b44f7853d6e5689bc
Matt York <my0373@gmail.com>
2023-05-21 22:35:42 +0100

    Added some user input sanitisation

--- src/Day 1 - Band Name Generator/bandname.py
+++ src/Day 1 - Band Name Generator/bandname.py
@@ -18,7 +18,7 @@ def main():
     band_dict['band_name'] = f"{band_dict['home_city']} {band_dict['pet_name']}"
 
     # Print the band na,e in quotes.
-    print(f" The band name is \"{band_dict['band_name'].capitalize()}\"")
+    print(f"The band name is \"{band_dict['band_name'].capitalize()}\"")
 
     # Exit with success (0)
     sys.exit(0)
@@ -28,12 +28,20 @@ def prompt_details(band_dict):
     Prompt the user on the command line to enter some personal information
     :return:
     """
+    # We create placeholders for the user data.
+    band_dict['home_city'] = ""
+    band_dict['pet_name'] = ""
 
-    # Prompt the user for their home city.
-    band_dict['home_city'] = input("Please enter your home city: ").lower()
+    # Prompt the user for their home city and pet name.
+    # If the response is empty, then we ask again until we get an answer.
+    # We remove all whitespace from the user input, and make it lowecase.
+
+    while not band_dict['home_city']:
+        band_dict['home_city'] = input("Please enter your home city: ").strip().lower()
 
     # Prompt the user for their first pets name.
-    band_dict['pet_name'] = input("Please enter your first pets name: ").lower()
+    while not band_dict['pet_name']:
+        band_dict['pet_name'] = input("Please enter your first pets name: ").strip().lower()
 
 if __name__ == '__main__':
     main()
